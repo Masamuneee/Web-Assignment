46a6adbb935acfdc76df6a90991fee8b2cdbce7a
Huy Nguyen Ngo <huy.nguyenngo13@hcmut.edu.vn>
2024-04-16 16:34:09 +0700

    Initialize home page

---
+++ my-app/src/components/navbar.js
@@ -0,0 +1,44 @@
+'use client'
+
+import { usePathname } from "next/navigation";
+import { ROUTES } from "@/constants/navigationRoutes";
+import { Button } from "@nextui-org/react";
+import Link from "next/link";
+
+export default function NavBar() {
+  const pathName = usePathname();
+  return (
+    <div className="w-full h-[3rem] bg-green-400">
+      <div className="max-w-screen-xl h-full mx-auto flex flex-row justify-between items-center">
+        <div>
+          <h1>Shopify!</h1>
+        </div>
+        <div className="h-full bg-inherit">
+          {ROUTES.navigationRoutes.map((route) => (
+            <Button
+              key={route}
+              radius="none"
+              className="bg-inherit h-full hover:bg-green-500"
+            >
+              {/* <Link href={`/${route}`}> */}
+              {route.charAt(0).toUpperCase() + route.slice(1)}
+              {/* </Link> */}
+            </Button>
+          ))}
+        </div>
+        <div>
+          <Button
+            radius="full"
+            color="primary"
+            size="sm"
+            // className="bg-inherit"
+          >
+            {/* <Link href="/login"> */}
+              Sign in
+            {/* </Link> */}
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ my-app/src/constants/navigationRoutes.js
@@ -0,0 +1,3 @@
+export const ROUTES = {
+  navigationRoutes: ["about", "products", "news", "contact"]
+}
