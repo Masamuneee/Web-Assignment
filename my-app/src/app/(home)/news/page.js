affd1ce029280b5c08947e9a3c22369f1d3a3838
Huy Nguyen Ngo <huy.nguyenngo13@hcmut.edu.vn>
2024-05-05 01:16:10 +0700

    More responsify News page

--- my-app/src/app/(home)/news/page.js
+++ my-app/src/app/(home)/news/page.js
@@ -1,25 +1,37 @@
 'use client'
 
-import NewsCard from "@/components/newsCard";
+import { NewsCard, NewsCardHero } from "@/components/newsCard";
 import { news } from "@/database/news";
 
 export default function NewsPage() {
-  const firstNews = news.shift();
+  const firstNews = news[0];
+  const remainingNews = news.slice(1);
   return (
     <div>
-      <div className="max-w-screen-xl mx-auto">
-        <h1 className="text-5xl text-center font-bold">LATEST UPDATES</h1>
-        <div className="mt-14 px-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 rounded-2xl">
-          {news.map((news) => (
-            <NewsCard
-              key={news.id}
-              category={news.category}
-              title={news.title}
-              description={news.description}
-              date={news.date}
-              image={news.image}
-            />
-          ))}
+      <div className="max-w-screen-xl mx-auto px-4">
+        <h1 className="home-section-header">LATEST UPDATES</h1>
+        <div className="mt-14 flex flex-col gap-8">
+          <NewsCardHero
+            className="mb-20"
+            key={firstNews.id}
+            category={firstNews.category}
+            title={firstNews.title}
+            description={firstNews.description}
+            date={firstNews.date}
+            image={firstNews.image}
+          />
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 rounded-2xl">
+            {remainingNews.map((news) => (
+              <NewsCard
+                key={news.id}
+                category={news.category}
+                title={news.title}
+                description={news.description}
+                date={news.date}
+                image={news.image}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </div>
