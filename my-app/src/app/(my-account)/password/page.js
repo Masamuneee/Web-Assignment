d0011116569c327baa4e8733816397d03c4fd872
Masamune <125840508+Masamuneee@users.noreply.github.com>
2024-05-06 14:15:46 +0700

    Update profile - Fix bug client && server

--- my-app/src/app/(my-account)/password/page.js
+++ my-app/src/app/(my-account)/password/page.js
@@ -2,8 +2,41 @@
 
 import { Input } from "@nextui-org/react"
 import { Button } from "@nextui-org/react"
+import React from "react";
+import { parseCookies } from "nookies";
+import jwt from 'jsonwebtoken'
+import axios from 'axios'
 
 export default function ChangePasswordPage() {
+  const [current_password, setCurrent_password] = React.useState("");
+  const [new_password, setNew_password] = React.useState("");
+  const [confirm_password, setConfirm_password] = React.useState("");
+  var id = 0
+
+  if (parseCookies().token && typeof window !== 'undefined') {
+    const decode = jwt.decode(parseCookies().token)
+    id = decode.user_id
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault()
+    const userData = {
+      id: id,
+      current_password,
+      new_password,
+      confirm_password
+    }
+
+    const response = await axios.post('http://localhost/test/users/password.php', userData)
+
+    if (response.data.status === 'success') {
+      alert(response.data.message);
+    }
+    else {
+      alert(response.data.message);
+    }
+  }
+  
   return (
     <div>
       <div>
@@ -13,21 +46,21 @@ export default function ChangePasswordPage() {
         <form method="POST" className="flex flex-col gap-5">
           <div className="grid grid-cols-[1.5fr_3fr] items-center gap-5">
             <label className="text-right">Current password</label>
-            <Input type="password" name="name"/>
+            <Input type="password" name="current_password" value={current_password} onChange={e => setCurrent_password(e.target.value)} />
           </div>
           <div className="grid grid-cols-[1.5fr_3fr] items-center gap-5">
             <label className="text-right">New password</label>
-            <Input type="password" name="name"/>
+            <Input type="password" name="new_password" value={new_password} onChange={e => setNew_password(e.target.value)} />
           </div>
           <div className="grid grid-cols-[1.5fr_3fr] items-center gap-5">
             <label className="text-right">Confirm password</label>
-            <Input type="password" name="name"/>
+            <Input type="password" name="confirm_password" value={confirm_password} onChange={e => setConfirm_password(e.target.value)} />
           </div>
         </form>
         <div>
 
         </div>
-        <Button color="primary">Submit</Button>
+        <Button color="primary" type="submit" onClick={handleSubmit}>Save changes</Button>
       </div>
     </div>
   )
--- my-app/src/utils/auth.js
+++ my-app/src/utils/auth.js
@@ -1,15 +1,16 @@
 import jwt from 'jsonwebtoken'
+import { parseCookies } from "nookies";
 
 export function checkAuth() {
   // Check if there is no cookie with the name 'token'
-  if (!document.cookie.split(';').some((item) => item.trim().startsWith('token='))) {
+  if (!parseCookies().token && typeof window !== 'undefined') {
     alert("You need to sign in to view this page");
     window.location.href = '/signin';
     return;
   }
   const cookie = document.cookie.split(';').find(cookie => cookie.startsWith('token=')).split('=')[1];
   const decoded_jwt = jwt.decode(cookie);
-  if (decoded_jwt.role !== 'admin') {
+  if (decoded_jwt.role !== 'admin' && typeof window !== 'undefined') {
     alert("You are not authorized to view this page");
     window.location.href = '/';
     return;
@@ -18,7 +19,7 @@ export function checkAuth() {
 
 export function checkAuthUser() {
   // Check if there is no cookie with the name 'token'
-  if (!document.cookie.split(';').some((item) => item.trim().startsWith('token='))) {
+  if (!parseCookies().token && typeof window !== 'undefined') {
     alert("You need to sign in to view this page");
     window.location.href = '/signin';
     return;
